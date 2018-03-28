aa90b133a41a909704f281f2c469bdac052e5d39
vagrant <vagrant@homestead>
2018-03-28 10:27:20 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','sample') - 新手入门教程</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+<div class="container">
+    @yield('content')
+    @include('layouts._footer')
+    {{--@include 是 Blade 提供的视图引用方法，可通过传参一个具体的文件路径名称来引用视图。--}}
+</div>
 </body>
 </html>
